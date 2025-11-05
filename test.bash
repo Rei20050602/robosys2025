f50626c1657e3933687120b4d2cb328c4224c494
rei <s24C1042LK@s.chibakoudai.jp>
2025-11-05 18:18:37 +0900

    Add a command

---
+++ test.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SPDX-FileCopyrightText:2025 Rei Kawahigashi
+
+ng () {
+        echo ${1}行目が違うよ
+        res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15.0 ] || ng "$LINENO"
+
+out=$(echo あ| ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "NINENO"
+[ "${out}" = "" ] || ng "LINENO"
+
+[ "${res}" = 0 ] && echo ok
+exit $res
