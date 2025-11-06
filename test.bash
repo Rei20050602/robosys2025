11882e0d1ca715030d055f317a73f2de45f5c9d3
rei <s24C1042LK@s.chibakoudai.jp>
2025-11-06 12:07:16 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SPDX-FileCopyrightText:2025 Rei Kawahigashi
+#SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
         echo ${1}行目が違うよ
@@ -20,4 +21,4 @@ out=$(echo | ./plus)
 [ "${out}" = "" ] || ng "LINENO"
 
 [ "${res}" = 0 ] && echo ok
-exit $res
+exit $resi
