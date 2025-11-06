a0ef754b959d5bc8ce660a504050f2394dc2f5d2
rei <s24C1042LK@s.chibakoudai.jp>
2025-11-06 12:31:26 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -17,8 +17,8 @@ out=$(echo あ| ./plus)
 [ "${out}" = "" ] || ng "$LINENO"
 
 out=$(echo | ./plus)
-[ "$?" = 1 ] || ng "LINENO"
-[ "${out}" = "" ] || ng "LINENO"
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo ok
 exit $resi
